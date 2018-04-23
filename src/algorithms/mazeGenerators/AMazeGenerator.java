e90965153592fbeaacddd5e2c0acc4a0bbac54f7
shawnseiref <shawnseiref@gmail.com>
2018-04-23 17:04:25 +0300

    adding AMazeGenerator timemilis

--- src/algorithms/mazeGenerators/AMazeGenerator.java
+++ src/algorithms/mazeGenerators/AMazeGenerator.java
@@ -10,6 +10,9 @@ public abstract class AMazeGenerator implements IMazeGenerator {
 
     @Override
     public long measureAlgorithmTimeMillis(int rows, int columns) {
-        return 0;
+        long startTime = System.currentTimeMillis();
+        generate(rows,columns);
+        long finishTime = System.currentTimeMillis();
+        return finishTime-startTime;
     }
 }
