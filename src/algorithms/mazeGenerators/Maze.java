756e63922f08a993dfb2f91b4502a0de0cacad47
shawnseiref <shawnseiref@gmail.com>
2018-04-23 17:56:59 +0300

    Maze 2nd implementation

--- src/algorithms/mazeGenerators/Maze.java
+++ src/algorithms/mazeGenerators/Maze.java
@@ -8,6 +8,23 @@ public class Maze {
     private Position startPos;
     private Position goalPos;
 
+
+    /**
+     * Maze constructor, initialize all slots as wall (1)
+     *                   and start and goal positions as (-1,-1)
+     * @param rows - number of rows the maze will have
+     * @param columns - number of columns the maze will have
+     */
+    public Maze (int rows, int columns){
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                maze [i][j]=1;
+            }
+        }
+        startPos = new Position(-1,-1);
+        goalPos = new Position(-1,-1);
+    }
+
     /**
      * get the start position
      * @return startPosition
