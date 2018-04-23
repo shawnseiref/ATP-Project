68230a08de6c8c55880d62498fdda12d36064fa1
shawnseiref <shawnseiref@gmail.com>
2018-04-23 16:58:49 +0300

    adding AMazeGenerator initial impliment

--- src/algorithms/mazeGenerators/AMazeGenerator.java
+++ src/algorithms/mazeGenerators/AMazeGenerator.java
@@ -1,4 +1,15 @@
 package algorithms.mazeGenerators;
 
-public abstract class AMazeGenerator {
+public abstract class AMazeGenerator implements IMazeGenerator {
+
+    public AMazeGenerator() {
+    }
+
+    @Override
+    public abstract Maze generate(int rows, int columns);
+
+    @Override
+    public long measureAlgorithmTimeMillis(int rows, int columns) {
+        return 0;
+    }
 }
