573814875d4b886837e0f31f050100d40cd21269
shawnseiref <shawnseiref@gmail.com>
2018-04-23 17:35:04 +0300

    Maze initial implementation

--- src/algorithms/mazeGenerators/Maze.java
+++ src/algorithms/mazeGenerators/Maze.java
@@ -1,6 +1,35 @@
 package algorithms.mazeGenerators;
 
+/**
+ * Maze class
+ */
 public class Maze {
+    private int [][] maze;
+    private Position startPos;
+    private Position goalPos;
 
+    /**
+     * get the start position
+     * @return startPosition
+     */
+    public Position getStartPosition(){
+        return startPos;
+    }
 
+    /**
+     * get the goal position
+     * @return goalPosition
+     */
+    public Position getGoalPos() {
+        return goalPos;
+    }
+
+    /**
+     * print the maze
+     * S - start Position
+     * E - exit Position
+     */
+    public void print(){
+
+    }
 }
--- src/algorithms/mazeGenerators/SimpleMazeGenerator.java
+++ src/algorithms/mazeGenerators/SimpleMazeGenerator.java
@@ -1,4 +1,10 @@
 package algorithms.mazeGenerators;
 
-public class SimpleMazeGenerator {
+public class SimpleMazeGenerator extends AMazeGenerator{
+
+    @Override
+    public Maze generate(int rows, int columns) {
+
+        return null;
+    }
 }
