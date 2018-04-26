2844db72c088eb62ec1caf8f192dae195feb4947
ilan <ilanpogreb01@gmail.com>
2018-04-26 15:15:20 +0300

    BFS Algorithm constructor

--- src/algorithms/search/BreathFirstSearch.java
+++ src/algorithms/search/BreathFirstSearch.java
@@ -1,4 +1,19 @@
 package algorithms.search;
 
-public class BreathFirstSearch {
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class BreathFirstSearch extends ASearchingAlgorithm{
+
+    public BreathFirstSearch(){
+        evaluatedNodes = new HashSet<>();
+        evaluatedNodesCount = 1;
+        neighbors = new ArrayList<>();
+        solution = new Solution();
+    }
+
+
+
+
+
 }
