1f51fd09d6fa6a8d5d994237e2db8734d5d33c3d
Shawn <shawn@post.bgu.ac.il>
2018-05-22 16:41:45 +0300

    part B

--- src/algorithms/mazeGenerators/Maze.java
+++ src/algorithms/mazeGenerators/Maze.java
@@ -4,36 +4,73 @@ package algorithms.mazeGenerators;
  * Maze class
  */
 public class Maze {
-    private int [][] grid;
+    private int[][] grid;
     private Position startPos;
     private Position goalPos;
 
+    /**
+     * Constructor, gets an uncompressed byte array that represents the maze              FIX!!
+     *
+     * @param b - uncompressed byte array
+     */
+    public Maze(byte[] b) {
+        int columns = bytesToInt(0, 1, b);
+        int rows = bytesToInt(2, 3, b);
+        grid = new int[rows][columns];
+        int tmpRow = bytesToInt(4, 5, b);
+        int tmpCol = bytesToInt(6, 7, b);
+        startPos = new Position(tmpRow, tmpCol);
+        tmpRow = bytesToInt(8, 9, b);
+        tmpCol = bytesToInt(10, 11, b);
+        goalPos = new Position(tmpRow, tmpCol);
+        int k = 12;
+        for (int i = 0; i < rows && k<b.length; i++) {
+            for (int j = 0; j < columns && k<b.length; j++) {
+                grid[i][j] = (int) b[k];
+                k++;
+            }
+        }
+    }
+
+    /**
+     * @param i
+     * @param j
+     * @param b
+     * @return
+     */
+    private int bytesToInt(int i, int j, byte[] b) {
+        int res = (b[i] & (0xff)) * 255;
+        res += (b[j] & (0xff));
+        return res;
+    }
 
     /**
      * Maze constructor, initialize all slots as wall (1)
-     *                   and start and goal positions as (-1,-1)
-     * @param rows - number of rows the maze will have
+     * and start and goal positions as (-1,-1)
+     *
+     * @param rows    - number of rows the maze will have
      * @param columns - number of columns the maze will have
      */
-    public Maze (int rows, int columns){
+    public Maze(int rows, int columns) {
         if (rows < 2) rows = 10;
         if (columns < 2) columns = 10;
         grid = new int[rows][columns];
         // CHECK IF ROWS OR COLUMNS >= 0 !!!!!
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
-                grid [i][j]=1;
+                grid[i][j] = 1;
             }
         }
-        startPos = new Position(-1,-1);
-        goalPos = new Position(-1,-1);
+        startPos = new Position(-1, -1);
+        goalPos = new Position(-1, -1);
     }
 
     /**
      * Constuctor for prepared grid and positions
+     *
      * @param preparedGrid - the grid to make maze of
-     * @param start - start position
-     * @param goal - goal position
+     * @param start        - start position
+     * @param goal         - goal position
      */
     public Maze(int[][] preparedGrid, Position start, Position goal) {
         grid = preparedGrid;
@@ -43,22 +80,25 @@ public class Maze {
 
     /**
      * get the maze' grid
-     * @return  int [][] grid
+     *
+     * @return int [][] grid
      */
-    public int[][] getGrid(){
+    public int[][] getGrid() {
         return grid;
     }
 
     /**
      * get the start position
+     *
      * @return startPosition
      */
-    public Position getStartPosition(){
+    public Position getStartPosition() {
         return startPos;
     }
 
     /**
      * get the goal position
+     *
      * @return goalPosition
      */
     public Position getGoalPosition() {
@@ -67,11 +107,12 @@ public class Maze {
 
     /**
      * set the Start position
-     * @param row - the new row value
+     *
+     * @param row    - the new row value
      * @param column - the new column value
      */
-    public void setStartPos(int row, int column){
-        if(getGoalPosition().getColumnIndex() == -1 && getGoalPosition().getRowIndex() == -1) {
+    public void setStartPos(int row, int column) {
+        if (getGoalPosition().getColumnIndex() == -1 && getGoalPosition().getRowIndex() == -1) {
             startPos.setRow(row);
             startPos.setColumn(column);
             grid[row][column] = 0;
@@ -81,11 +122,12 @@ public class Maze {
 
     /**
      * set the Goal position
-     * @param row - the new row value
+     *
+     * @param row    - the new row value
      * @param column - the new column value
      */
-    public void setGoalPos(int row, int column){
-        if(getGoalPosition().getColumnIndex() == -1 && getGoalPosition().getRowIndex() == -1) {
+    public void setGoalPos(int row, int column) {
+        if (getGoalPosition().getColumnIndex() == -1 && getGoalPosition().getRowIndex() == -1) {
             goalPos.setRow(row);
             goalPos.setColumn(column);
             grid[row][column] = 0;
@@ -94,6 +136,7 @@ public class Maze {
 
     /**
      * set the grid
+     *
      * @param grid - the new grid to use
      */
     public void setGrid(int[][] grid) {
@@ -105,13 +148,13 @@ public class Maze {
      * S - start Position
      * E - exit Position
      */
-    public void print(){
+    public void print() {
         int s_row = getStartPosition().getRowIndex();
         int s_column = getStartPosition().getColumnIndex();
         int e_row = getGoalPosition().getRowIndex();
         int e_column = getGoalPosition().getColumnIndex();
-        for (int i = 0; i < grid.length; i++){
-            for (int j = 0; j< grid[0].length; j++){
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
                 if (i == s_row && j == s_column)
                     System.out.print("S");
                 else if (i == e_row && j == e_column)
