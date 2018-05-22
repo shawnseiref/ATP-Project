3f1534bb567e97d360a01cab9ec1e960f7226a04
ilan <ilanpogreb01@gmail.com>
2018-05-22 19:08:06 +0300

    MyDecompressorOutputStream.read(byte[]) implementation, still unDocd

--- src/IO/MyDecompressorInputStream.java
+++ src/IO/MyDecompressorInputStream.java
@@ -7,12 +7,32 @@ public class MyDecompressorInputStream extends InputStream {
 
     private InputStream in;
 
-    MyDecompressorInputStream(InputStream inputStream){
+    MyDecompressorInputStream(InputStream inputStream) {
         in = inputStream;
     }
 
     @Override
     public int read() throws IOException {
+        return in.read();
+    }
+
+    public int read(byte[] b) throws IOException {
+        if (b == null)
+            throw new NullPointerException();
+        if (b.length < 13)
+            throw new ArrayIndexOutOfBoundsException();
+
+        int i;
+        for (i = 0; i < 13; i++)
+            b[i] = (byte) read();
+
+        for (int counter = read() , evenIndicator = -1, wallOrPass; counter != -1; counter = read()) {
+            evenIndicator++;
+            wallOrPass = (evenIndicator % 2 == 0) ? 0 : 1;
+            for (; counter > 0; i++, counter--) {
+                b[i] = (byte) wallOrPass;
+            }
+        }
         return 0;
     }
 }
